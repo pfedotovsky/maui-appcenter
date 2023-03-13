e2eca364b389764a32c7665f8efaa6826d9af908
pfedotovsky <pavel.fedotovsky@outlook.com>
2023-03-13 13:31:44 +0400

    Add empty MAUI app

---
+++ MAUI-AppCenter/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUI_AppCenter;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ MAUI-AppCenter/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MAUI_AppCenter;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
