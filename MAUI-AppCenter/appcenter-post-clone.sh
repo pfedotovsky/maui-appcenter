e6a173240326533d3deb0a4bf3587c8aecbb720f
pfedotovsky <pavel.fedotovsky@outlook.com>
2023-03-13 16:09:35 +0400

    Attempt to use dotnet as nuget

--- MAUI-AppCenter/appcenter-post-clone.sh
+++ MAUI-AppCenter/appcenter-post-clone.sh
@@ -5,11 +5,11 @@ echo "CPU architecture: $(uname -m)"
 echo "dotnet version: $(dotnet --version)"
 
 # use msbuild from .NET SDK
-echo """#!/bin/sh
-exec dotnet build \"\$@\"
-""" | sudo tee /Library/Frameworks/Mono.framework/Commands/msbuild
-
-# use nuget as dotnet
 # echo """#!/bin/sh
-# exec dotnet \"\$@\"
-# """ | sudo tee /Library/Frameworks/Mono.framework/Commands/nuget
+# exec dotnet build \"\$@\"
+# """ | sudo tee /Library/Frameworks/Mono.framework/Commands/msbuild
+
+# use dotnet as nuget
+echo """#!/bin/sh
+exec dotnet
+""" | sudo tee /Library/Frameworks/Mono.framework/Commands/nuget
