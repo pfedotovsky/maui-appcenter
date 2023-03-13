b48d0b0b92d77ebc100489213d2dc07f319eaf82
pfedotovsky <pavel.fedotovsky@outlook.com>
2023-03-13 17:33:26 +0400

    Use global dotnet SDK

--- MAUI-AppCenter/appcenter-post-clone.sh
+++ MAUI-AppCenter/appcenter-post-clone.sh
@@ -3,6 +3,7 @@
 echo "Post-clone script executing..."
 echo "CPU architecture: $(uname -m)"
 echo "dotnet version: $(dotnet --version)"
+echo "dotnet path: $(which dotnet)"
 
 # Install MAUI iOS workload
 dotnet workload install maui-ios
@@ -14,5 +15,8 @@ dotnet workload install maui-ios
 
 # use dotnet as nuget
 echo """#!/bin/sh
+which dotnet
+PATH="/usr/local/share/dotnet:$PATH"
+which dotnet
 exec dotnet restore 'MAUI-AppCenter/MAUI-AppCenter.sln'
 """ | sudo tee /Library/Frameworks/Mono.framework/Commands/nuget
