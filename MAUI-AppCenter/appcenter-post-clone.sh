d64251e28bd78264c242de8f53adf2e30019c083
pfedotovsky <pavel.fedotovsky@outlook.com>
2023-03-13 13:48:51 +0400

    Add post-clone script

---
+++ MAUI-AppCenter/appcenter-post-clone.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "Post-clone script executing..."
+echo "CPU architecture: $(uname -m)"
+echo "dotnet version: $(dotnet --version)"
+
+# use nuget as dotnet
+echo """#!/bin/sh
+exec dotnet \"\$@\"
+""" | sudo tee /Library/Frameworks/Mono.framework/Commands/nuget
