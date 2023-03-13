da3526458ee71e0f455f5f3c770c9d24959286da
pfedotovsky <pavel.fedotovsky@outlook.com>
2023-03-13 14:08:32 +0400

    Use msbuild from .NET SDK

--- MAUI-AppCenter/appcenter-post-clone.sh
+++ MAUI-AppCenter/appcenter-post-clone.sh
@@ -4,6 +4,11 @@ echo "Post-clone script executing..."
 echo "CPU architecture: $(uname -m)"
 echo "dotnet version: $(dotnet --version)"
 
+# use msbuild from .NET SDK
+echo """#!/bin/sh
+exec dotnet build \"\$@\"
+""" | sudo tee /Library/Frameworks/Mono.framework/Commands/msbuild
+
 # use nuget as dotnet
 # echo """#!/bin/sh
 # exec dotnet \"\$@\"
