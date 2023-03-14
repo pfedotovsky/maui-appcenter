d9aeeb9621d7a190c4875a6bb8accebba5a65f05
pfedotovsky <pavel.fedotovsky@outlook.com>
2023-03-14 15:52:46 +0400

    Improve dummy msbuild

--- MAUI-AppCenter/appcenter-post-clone.sh
+++ MAUI-AppCenter/appcenter-post-clone.sh
@@ -4,6 +4,7 @@ echo "Post-clone script executing..."
 echo "CPU architecture: $(uname -m)"
 echo "dotnet version: $(dotnet --version)"
 echo "dotnet path: $(which dotnet)"
+echo "msbuild version: $(msbuild --version)"
 
 # Install MAUI iOS workload
 dotnet workload install maui-ios
@@ -13,7 +14,7 @@ dotnet build
 
 # Use dummy msbuild
 echo """#!/bin/sh
-echo Dummy Build
+echo $(msbuild --version)
 """ | sudo tee /Library/Frameworks/Mono.framework/Commands/msbuild
 
 # Use dummy nuget
