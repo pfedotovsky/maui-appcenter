28b49b3ad23e6d77c8187ea26ded02aef8ca7c34
pfedotovsky <pavel.fedotovsky@outlook.com>
2023-03-15 01:02:37 +0400

    Set ApplicationVersion to timestamp

--- MAUI-AppCenter/appcenter-post-clone.sh
+++ MAUI-AppCenter/appcenter-post-clone.sh
@@ -17,7 +17,7 @@ dotnet workload restore
 # dotnet build --configuration $APPCENTER_XAMARIN_CONFIGURATION
 
 # Create release ipa for devices
-dotnet publish -f:net7.0-ios -c:Release /p:ArchiveOnBuild=true /p:RuntimeIdentifier=ios-arm64 /p:CodesignKey="$APPLE_CERTIFICATE_SIGNING_IDENTITY"
+dotnet publish -f:net7.0-ios -c:Release /p:ArchiveOnBuild=true /p:RuntimeIdentifier=ios-arm64 /p:CodesignKey="$APPLE_CERTIFICATE_SIGNING_IDENTITY" /p:ApplicationVersion=$(date +%s)
 
 # Use dummy msbuild
 echo """#!/bin/sh
