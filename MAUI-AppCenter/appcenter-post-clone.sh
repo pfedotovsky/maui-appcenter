23c275042e06327a394aeba18697e67dd25843ff
pfedotovsky <pavel.fedotovsky@outlook.com>
2023-03-18 12:33:45 +0400

    Disable AppCenter build tasks

--- MAUI-AppCenter/appcenter-post-clone.sh
+++ MAUI-AppCenter/appcenter-post-clone.sh
@@ -4,30 +4,12 @@ dotnet --info
 
 # Log diagnostics information
 echo "build configuration: $APPCENTER_XAMARIN_CONFIGURATION"
-echo "nuget is $(which nuget)"
-echo "msbuild is $(which msbuild)"
-echo "BASH_ENV is $BASH_ENV"
 echo "PATH is $PATH"
 
-# Set up BASH_ENV to override nuget; In AppCenter BASH_ENV must be set to /Users/runner/AppCenter/bash_env
-dirName=$(dirname $BASH_ENV)
-mkdir $dirName
-echo "export PATH=$dirName:$PATH" > $BASH_ENV
-echo "echo Dummy Nuget" > $dirName/nuget
-chmod +x $dirName/nuget
-
-# source $BASH_ENV
-# echo "nuget now is $(which nuget)"
-
-# Use dummy msbuild; 'echo 15' required to prevent fallback to xbuild as build task check for msbuild version 15 and above
-echo """#!/bin/sh
-echo 15
-""" | sudo tee /Library/Frameworks/Mono.framework/Commands/msbuild
-
-# Disable several redundant AppCenter tasks
-# echo -n "" > $(find $AGENT_ROOTDIRECTORY -name 'usedotnet.js')
-# echo -n "" > $(find $AGENT_ROOTDIRECTORY -name 'xamarinios.js')
-# echo -n "" > $(find $AGENT_ROOTDIRECTORY -name 'cmdlinetask.js')
+# Disable AppCenter build tasks
+echo -n "" > $(find $AGENT_ROOTDIRECTORY -name 'usedotnet.js')
+echo -n "" > $(find $AGENT_ROOTDIRECTORY -name 'xamarinios.js')
+echo -n "" > $(find $AGENT_ROOTDIRECTORY -name 'cmdlinetask.js')
 
 # Install MAUI workloads
 dotnet workload restore
